26b5ba18f2f9f7baaecd1e95e913b9af71256a75
Lopez <HLopez49@cps.k12.il.us>
2024-09-12 14:40:39 -0500

    using math commands

--- week3_day_1.py
+++ week3_day_1.py
@@ -9,8 +9,8 @@
 
 
 
-# # 1.   Building a Basic Calculator
-# # 2.   Mad Libs Game
+# # # 1.   Building a Basic Calculator
+# # # 2.   Mad Libs Game
 
 
 
@@ -47,43 +47,43 @@
 
 
 
-# # Review
-# create variables for the following :
-# 1. age
-age = 18 
-# interger variable
-# 2. name
-name = "Hector"
-# string var
-# # 3. song
-song = "crab rave"
-# 4. food
-food = "chicken tendies"
-# 5. number
-number = 100
+# # # # Review
+# # # create variables for the following :
+# # # 1. age
+# # age = 18 
+# # # interger variable
+# # # 2. name
+# # name = "Hector"
+# # # string var
+# # # # 3. song
+# # song = "crab rave"
+# # # 4. food
+# # food = "chicken tendies"
+# # # 5. number
+# # number = 100
 
 
-# #now include the variables you just made print in the following...
-print("You are" + str(age) + " years old" + "your name is" + name + "Your favorite song is" + song + "your favorite number is" + str(number))
+# # # #now include the variables you just made print in the following...
+# # print("You are" + str(age) + " years old" + "your name is" + name + "Your favorite song is" + song + "your favorite number is" + str(number))
 
-# Once upon a time, there was a [age] old coder named [name].
-print(" once upon a time there was a " + str(age) + " old coder named " + name + ".")
+# # # Once upon a time, there was a [age] old coder named [name].
+# # print(" once upon a time there was a " + str(age) + " old coder named " + name + ".")
 
-# [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
-print(name + " liked to hum the song" + song + " while coding. It was so annoying that their teammates would throw " + food + " until " + name + " would stop singing")
+# # # [name] liked to hum the song [song] while coding. It was so annoying that their teammates would throw [food] until [name] would stop singing.
+# # print(name + " liked to hum the song" + song + " while coding. It was so annoying that their teammates would throw " + food + " until " + name + " would stop singing")
 
-# Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
-print(" Still, " + name + " was the best coder on the team and could write " + str(number) + " lines of code every day. Maybe " + song + " was " + name + " 's secret power")
-##########################################################################################
+# # # Still, [name] was the best coder on the team and could write [number] lines of code every day. Maybe [song] was [name]’s secret power?
+# # print(" Still, " + name + " was the best coder on the team and could write " + str(number) + " lines of code every day. Maybe " + song + " was " + name + " 's secret power")
+# # ##########################################################################################
 
-number1 = 100
-number2 = 200
-number3 = 300
-number4 = 400
-number5 = 500
-# print(number1, + number2, number3, number4, number5)
-# f string must use squigly brackets
-print(f"{number1}, {number2}, {number3}, {number4}, {number5}")
+# # number1 = 100
+# # number2 = 200
+# # number3 = 300
+# # number4 = 400
+# # number5 = 500
+# # # print(number1, + number2, number3, number4, number5)
+# # # f string must use squigly brackets
+# # print(f"{number1}, {number2}, {number3}, {number4}, {number5}")
 
 
 
@@ -93,54 +93,54 @@ print(f"{number1}, {number2}, {number3}, {number4}, {number5}")
 
 
 
-##########################################################################################
-# The names you use when creating these labels need to follow a few rules:
-# 1. Names can not start with a number.
-# 2. There can be no spaces in the name, use _ instead.
-# 3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
-# 4. It's considered best practice (PEP8) that names are lowercase.
-# 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh),
-#    or 'I' (uppercase letter eye) as single character variable names.
-# 6. Avoid using words that have special meaning in Python like "list" and "str"
+# ##########################################################################################
+# # The names you use when creating these labels need to follow a few rules:
+# # 1. Names can not start with a number.
+# # 2. There can be no spaces in the name, use _ instead.
+# # 3. Can't use any of these symbols :'",<>/?|\()!@#$%^&*~-+
+# # 4. It's considered best practice (PEP8) that names are lowercase.
+# # 5. Avoid using the characters 'l' (lowercase letter el), 'O' (uppercase letter oh),
+# #    or 'I' (uppercase letter eye) as single character variable names.
+# # 6. Avoid using words that have special meaning in Python like "list" and "str"
 
 
-# Here are some exercises to practice the rules:
+# # Here are some exercises to practice the rules:
 
 
-# Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
+# # Correcting Invalid Names: Below are some invalid names. Correct them according to the rules:
 
 
-# 1st_name first_name*
-# last name last_name*
-# email@address email_address*
-# percent% percent*
-# variable#name variable_name*
-# O zero*
-# list list*
-# Creating Valid Names: Create valid names for the following descriptions:
+# # 1st_name first_name*
+# # last name last_name*
+# # email@address email_address*
+# # percent% percent*
+# # variable#name variable_name*
+# # O zero*
+# # list list*
+# # Creating Valid Names: Create valid names for the following descriptions:
 
 
-# The first name of a person
-# The last name of a person
-# The email address of a person
-# The percentage of marks obtained by a student
-# A variable to store the number of items in a shopping cart
+# # The first name of a person
+# # The last name of a person
+# # The email address of a person
+# # The percentage of marks obtained by a student
+# # A variable to store the number of items in a shopping cart
 
 
 
 
-# Identify Valid and Invalid Names: Identify which of the following names are valid or invalid according to the rules:
+# # Identify Valid and Invalid Names: Identify which of the following names are valid or invalid according to the rules:
 
 
-# first_name
-# lastName
-# email_address
-# percentage
-# variable_name
-# 1_variable
-# email@address email_address*
-# percentage% percentage*
-# i works but needs more description
+# # first_name
+# # lastName
+# # email_address
+# # percentage
+# # variable_name
+# # 1_variable
+# # email@address email_address*
+# # percentage% percentage*
+# # i works but needs more description
 
 
 
@@ -149,46 +149,66 @@ print(f"{number1}, {number2}, {number3}, {number4}, {number5}")
 
 
 
+# ############################################################################################
 
 
+# # # **Working with** **numbers** **bold text**
+# # We'll learn about the following topics:
+# # 1. Types of Numbers in Python
+# # 2. Basic Arithmetic
+# # 3. Differences between classic division and floor division
 
 
-
-
-
-
-############################################################################################
-
-
-# # **Working with** **numbers** **bold text**
-# We'll learn about the following topics:
-# 1. Types of Numbers in Python
-# 2. Basic Arithmetic
-# 3. Differences between classic division and floor division
-
-
-# Python has various "types" of numbers (numeric literals).
-# 1. We'll mainly focus on integers and floating point numbers.
-# Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
-# 2. Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
+# # Python has various "types" of numbers (numeric literals).
+# # 1. We'll mainly focus on integers and floating point numbers.
+# # Integers are just whole numbers, positive or negative. For example: 2 and -2 are examples of integers.
+# # 2. Floating point numbers in Python are notable because they have a decimal point in them, or use an exponential (e) to define the number. For example 2.0 and -2.1 are examples of floating point numbers. 4E2 (4 times 10 to the power of 2) is also an example of a floating point number in Python.
 
 
 ##########################################################################################
-# #addition
+# #addition 
+print(2+1)
 # #multiplication
+print(2*2)
 # #division
+print(3/2)
+
 # #modulo
+print(7%4)
+# looking for the remainder of two numbers divided
+
 # #powers
+print(2**3)
+# exponent
+
 # #get the max and min of a number
+print(max(1,2))
+print(min(1,2))
+# max and min are reserved for python u can't use them as variables
+
 # #round a number
+print(round(3,4))
+
 # # absolute value
+print(abs(-3))
+# value is the distsance from 0 to the number
 # # order of operations
+print(2 + 10 * 10 + 3)
+
 # #to do more you need to import special math libraries from python
-# #from math import *    
+from math import *    
 # #this goes out and grabs some different math functions we can use
 # #floor method
+print(floor(3.7))
+# rounds up to whole number
 # #ceil method
+print(ceil(3.7))
+
 # #sqrt method
+print(sqrt(36))
+
+# these 3 will not work without the math libarary
+
 
 
 
@@ -208,14 +228,23 @@ print(f"{number1}, {number2}, {number3}, {number4}, {number5}")
 # # **Getting Input from users**
 # #how do we get input from users?
 # input("what is your name?")
+name= input("what is your name")
+print("hello" + name)
 # # basic math calculator
 # #ask the user for 2 numbers
+num1= int(input("enter a number: "))
+num2= int(input("enter another number:"))
 # # print out a statement where you:
 # # add them together
+print (num1 + num2)
 # #multiply
+print(num1 * num2)
 # # find the max number
+print(max(num1,num2))
 # # find the remainder of the numbers
+print(num1%num2)
 # #round one number
+print(ceil(num1%num2))
 
 
 
